fe3450277348ac77073aac187ac59a518e6d3b4a
LabraxAI <michel-bartels@web.de>
2017-08-02 17:00:50 +0200

    Added possibility of adding and subtracting scalars to vectors

--- vector.js
+++ vector.js
@@ -5,30 +5,46 @@ function vector(array) {
 vector.prototype.show = function () {
     console.table(this.array)
 }
-vector.prototype.add = function (array) {
-    if (Object.prototype.toString.call(array) != "array" && !(array instanceof vector)){
+vector.prototype.substract = function (array) {
+    var is_array = Object.prototype.toString.call(array) == "[object Array]"
+    var is_number = typeof array == "number"
+    if (!(is_array) && !(array instanceof vector) && !(is_number)){
         throw "Can't add object of this type"
     } else {
         if (this.array.length != array.length) {
             console.warn("Length of array is not the same")
         }
-        for (var i = 0; i < this.array.length; i++) {
-            if (array[i] != undefined) {
-                this.array[i] += array[i]
+        if (is_number) {
+            for (var i = 0; i < this.array.length; i++) {
+                this.array[i] += array
+            }
+        } else {
+            for (var i = 0; i < this.array.length; i++) {
+                if (array[i] != undefined) {
+                    this.array[i] += array[i]
+                }
             }
         }
     }
 }
 vector.prototype.substract = function (array) {
-    if (Object.prototype.toString.call(array) != "array" && !(array instanceof vector)){
+    var is_array = Object.prototype.toString.call(array) == "[object Array]"
+    var is_number = typeof array == "number"
+    if (!(is_array) && !(array instanceof vector) && !(is_number)){
         throw "Can't add object of this type"
     } else {
         if (this.array.length != array.length) {
             console.warn("Length of array is not the same")
         }
-        for (var i = 0; i < this.array.length; i++) {
-            if (array[i] != undefined) {
-                this.array[i] -= array[i]
+        if (is_number) {
+            for (var i = 0; i < this.array.length; i++) {
+                this.array[i] -= array
+            }
+        } else {
+            for (var i = 0; i < this.array.length; i++) {
+                if (array[i] != undefined) {
+                    this.array[i] -= array[i]
+                }
             }
         }
     }
