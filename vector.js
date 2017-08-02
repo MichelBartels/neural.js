6470fd5d831a13df01f4cb2c3a5e845b0c4e4cac
LabraxAI <michel-bartels@web.de>
2017-08-02 22:27:52 +0200

    Added multiplication of vectors

--- vector.js
+++ vector.js
@@ -2,6 +2,13 @@ function vector(array) {
     this.array = array
     this.dimensions = array.length
 }
+vector.prototype.length = function () {
+    var sum = 0
+    for (var i = 0; i < this.array.length; i++) {
+        sum += Math.pow(this.array[i], 2)
+    }
+    return Math.sqrt(sum)
+};
 vector.prototype.show = function () {
     if (window.chrome) {
         var temp = []
@@ -47,34 +54,34 @@ vector.prototype.add = function (to_add) {
         }
     }
 }
-vector.prototype.add = function (to_add) {
-    var is_array = Object.prototype.toString.call(to_add) == "[object Array]"
-    var is_number = typeof to_add == "number"
-    if (!(is_array) && !(to_add instanceof vector) && !(is_number)){
-        throw "Can't add object of this type"
+vector.prototype.subtract = function (to_subtract) {
+    var is_array = Object.prototype.toString.call(to_subtract) == "[object Array]"
+    var is_number = typeof to_subtract == "number"
+    if (!(is_array) && !(to_subtract instanceof vector) && !(is_number)){
+        throw "Can't subtract object of this type"
     } else {
         if (is_array) {
-            if (this.array.length != to_add.length) {
+            if (this.array.length != to_subtract.length) {
                 console.warn("Length of vector is not the same")
             }
         } else {
-            if (this.array.length != to_add.array.length) {
+            if (this.array.length != to_subtract.array.length) {
                 console.warn("Length of vector is not the same")
             }
         }
         if (is_number) {
             for (var i = 0; i < this.array.length; i++) {
-                this.array[i] -= to_add
+                this.array[i] -= to_subtract
             }
         } else {
             for (var i = 0; i < this.array.length; i++) {
                 if (is_array) {
-                    if (to_add[i] != undefined) {
-                        this.array[i] -= to_add[i]
+                    if (to_subtract[i] != undefined) {
+                        this.array[i] -= to_subtract[i]
                     }
                 } else {
-                    if (to_add.array[i] != undefined) {
-                        this.array[i] -= to_add.array[i]
+                    if (to_subtract.array[i] != undefined) {
+                        this.array[i] -= to_subtract.array[i]
                     }
                 }
             }
@@ -103,4 +110,11 @@ vector.prototype.multiply = function (to_multiply) {
             }
         }
     }
+}
+vector.prototype.normalize = function () {
+    var temp = []
+    for (var i = 0; i < this.array.length; i++) {
+        temp[i] = this.array[i] / this.length()
+    }
+    this.array = temp
 }
